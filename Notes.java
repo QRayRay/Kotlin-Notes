6745ff6fb538a8c60b119c2ac8769891b4655036
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-04-06 19:48:18 -0500

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -50,3 +50,16 @@ fun addUp(a: Int, b: Int): Int{
 fun avg(a: Double, b: Double): Double {
     return  (a + b)/2
 }
+
+
+fun main(){
+    val iphone = MobilePhone("iOS", "Apple", "iPhone 12")
+    val galaxyS20 = MobilePhone("Android","Samsung", "Galaxy S20")
+    val mateXS = MobilePhone("Android", "Huawei", "Mate X S")
+}
+ 
+class MobilePhone(osName: String, brand: String, model: String){
+    init {
+        println("The phone $model from $brand uses $osName as its Operating System")
+    }
+}
