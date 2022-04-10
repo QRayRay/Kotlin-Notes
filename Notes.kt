8a9fe574379a9e896a9c315c456ad1d09693563f
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-04-10 15:38:58 -0500

    Update Notes.kt

--- Notes.kt
+++ Notes.kt
@@ -169,3 +169,15 @@ class Car() {
         this.owner = "Frank"
     }
 }
+
+open class Polygon {
+    open fun draw() {
+        // some default polygon drawing method
+    }
+}
+
+abstract class WildShape : Polygon() {
+    // Classes that inherit WildShape need to provide their own
+    // draw method instead of using the default on Polygon
+    abstract override fun draw()
+}
